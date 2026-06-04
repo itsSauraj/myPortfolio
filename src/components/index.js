557345f1ddf81d502684ca6c18caebe295a0ad6b
Saurabh Yadav <sauraj.contact@gmail.com>
2026-06-04 21:07:49 +0530

    feat: remove Earth component and related assets; update canvas exports accordingly

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
-import { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas } from './canvas';
+import { BallCanvas, ComputersCanvas, StarsCanvas } from './canvas';
 import Hero from './Hero';
 import Navbar from './Navbar';
 import About from './About';
@@ -26,7 +26,6 @@ export {
     Works,
     Feedbacks,
     Contact,
-    EarthCanvas,
     BallCanvas,
     ComputersCanvas,
     StarsCanvas,
